a3d6b758aedfdf1987efd8a3deac60b74ec86f61
Spidey_Cat <63194910+Maniprogramer@users.noreply.github.com>
2023-07-31 08:57:04 +0530

    Update Gsm_module_with_P10_Display.ino

--- Gsm_module_with_P10_Display.ino
+++ Gsm_module_with_P10_Display.ino
@@ -1,22 +1,20 @@
-#include <SPI.h>       
+ #include <SPI.h>       
 #include <DMD.h>    
 #include <TimerOne.h>  
 #include "Arial_black_16.h"
+#include "Arial_Black_16_ISO_8859_1.h"
+#include "Arial14.h"
 #include "SystemFont5x7.h"
 
 #define DISPLAYS_ACROSS 1 //-> Number of P10 panels used, side to side.
 #define DISPLAYS_DOWN 1
 DMD dmd(DISPLAYS_ACROSS, DISPLAYS_DOWN);
 
-const int BUTTON_PIN1 = 2;
-int buttonState1 = 0;
-
 char *Text = "";
-char startup[] = "Starting GSM Module"; 
 
 void ScanDMD() { 
   dmd.scanDisplayBySPI();
-} 
+}
 
 
 int led=13;
@@ -24,18 +22,27 @@ int led=13;
 int temp=0,i=0,x=0,k=0,sent=0,first=0;
 char str[200],msg[64];
 
+#define BUF_SIZE 75
 
-void setup(void) {
-  
-  Timer1.initialize(5000);          
+
+char startup[] = "Starting GSM Module";                         //This is variable used to initialise the starting message.
+//char 
+
+
+
+
+
+void setup() {
+  Timer1.initialize(1000);          
   Timer1.attachInterrupt(ScanDMD);   
-  dmd.clearScreen(true);   
-  pinMode(BUTTON_PIN1, INPUT_PULLUP);
-  Serial.begin(115200);
+  dmd.clearScreen(true);
   
+  Serial.begin(9600);   //serial monitor baud rate
+
+//Initialising the gsm module
   Serial.println("GSM Initilizing...");
+
   gsm_init();                           //calling the gsm_initialising funtion
-  
   Serial.println("Wireless Notice");
   delay(200);
   Serial.println("    Board      ");
@@ -45,84 +52,58 @@ void setup(void) {
   delay(500);
   Serial.println("AT+CMGF=1");            //Gsm module is set to the text mode
   delay(500);
-
-  strcpy (msg,startup);
-  p10();
-  
 }
 
-void loop(void) {
-  buttonState1 = digitalRead(BUTTON_PIN1);
-  
-  if(buttonState1 == LOW){
-    p10();
-    Serial.println("Button clicked");
-   }
+void loop() {
 
-  for(unsigned int t=0; t<60000; t++) {
-    serialEvent(); //calling serial event funtion
-    if(temp==1) {
-      dmd.clearScreen (true); 
-      x=0, k=0, temp=0;
-      while(x < i) {
-        while(str[x] == '#') { // checks for #(hash) in the message if it is present it starts storing the message
-          x++;
-          while(str[x] != '*') { // checks for *(asterisk) in the message if it is present it stops storing the message
-            msg[k++] = str[x++];
-          }
-        }
+    for(unsigned int t=0;t<60000;t++)
+  {
+    serialEvent();                    //calling serial event funtion
+  if(temp==1)
+  {
+    x=0,k=0,temp=0;
+    while(x<i)
+    {
+      while(str[x]=='#')            //checks for #(hash) in the message if it is present its start storing the message
+      {
         x++;
+        while(str[x]!='*')          //checks for *(astrix) in the message if it is present it stops storing the message
+        {
+          msg[k++]=str[x++];
+        }
       }
-      msg[k] = '\0';
- 
-//       Text = msg; 
-      if(msg==""){
-        strcpy (msg,"No Recent Messages");
-        p10();
-      }
-      Serial.println("Str:");
-      Serial.print(str);
-      Serial.println("Msg:");
-      Serial.println(msg); //print the received message in the serial monitor
-      
-      if(sent == 1) {
-        if(msg!=""){
-          sent = 0;
-           SendMessage(); //calling the send message funtion to send msg when the message is received from the receiver || someone
-         }else{
-          Serial.println ("Sending Message");
-          Serial.println("AT+CMGF=1");        //Sets the GSM Module in Text Mode
-          delay(1000);
-          Serial.println ("Set SMS Number");
-          Serial.println("AT+CMGS=\"+918686873899\"\r"); //Mobile phone number to send message
-          delay(1000);
-          Serial.println("Message not Received");   // Messsage content
-          delay(1000);
-          }
-      }
-      
-      p10();
-//      delay(1000);
+      x++;
+    }
+    msg[k]='\0';
+    Serial.println("str:");
+    Serial.println(str);
+    Serial.println("msg:");
+    Serial.println(msg);      //print the received message in serial monitor
+    p10();
+    delay(1000);
+    temp=0;
+    i=0;
+    x=0;
+    k=0;
 
-      temp=0;
-      i=0;
-      x=0;
-      k=0;
+   if(sent==1){
+      sent=0;
+      SendMessage();    //calling the send message funtion to sending msg when message is received from the receiver || someone
+          
+      }
     }
   }
-
 }
-
 void p10(){
-        dmd.selectFont(Arial_Black_16);
-        dmd.drawMarquee(msg,strlen(msg),(32*DISPLAYS_ACROSS)-1,0);
-        boolean ret=false;
-        while(!ret){
-          ret=dmd.stepMarquee(-1,0);
-          delay(100);
-    }
+  dmd.selectFont(Arial_Black_16);
+  //-----------------------------------------------------------By using "delay()"
+  dmd.drawMarquee(msg,strlen(msg),(32*DISPLAYS_ACROSS)-1,0);
+  boolean ret=false;
+  while(!ret){
+    ret=dmd.stepMarquee(-1,0);
+    delay(100);
   }
-
+  }
 void serialEvent()      //This funtion checks *(astrix) if it is present it declares as message received
 {
   while(Serial.available())
@@ -133,6 +114,7 @@ void serialEvent()      //This funtion checks *(astrix) if it is present it decl
     {
       temp=1;
       sent=1;
+      first=1;
 
       Serial.println("Message Received");     //This message is printed in serial monitor
       delay(500);                             //waits for a half second
@@ -197,5 +179,8 @@ void gsm_init()               //This funtion is to initialise the gsm module
   delay(1000);
   Serial.println("Message Received :");   // Messsage content
   Serial.print(msg);   // Messsage content
+  delay(100);
+
+  Serial.println((char)26);// ASCII code of CTRL+Z
   delay(1000);
 }
